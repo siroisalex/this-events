d4246adb501ee8dae5c875562e32cc7bad9cfc97
Alexandra Sirois <AlexandraSirois@Alexandras-MacBook-Pro.local>
2017-06-26 21:55:08 -1000

    completed assignment

--- js/app.js
+++ js/app.js
@@ -1,4 +1,41 @@
 //Secret Menu Challenge!
 /*Add event listeners to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
 
+var nameElem = document.getElementsByClassName("name");
+
+for(i=0; i<nameElem.length; i++){
+nameElem[i].addEventListener("click", showMenu);
+}
+
+function showMenu(){
+  var menuElem = this.querySelectorAll(".menu")[0];
+  if(menuElem.style.display === "none"){
+    menuElem.style.display = "block";
+  }else{
+    menuElem.style.display = "none";
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
